0f3f5480951aedb4cde93bedf198effb7aa742a1
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-01-17 14:19:07 -0300

    feat(register): add function to register new user to server

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import * as auth from "../utils/auth";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   async function handleSubmit(e) {
     e.preventDefault();
     try {
       const res = await auth.signup({ email, password });
-      console.log(res);
+      if (res) {
+        navigate("/login");
+      }
     } catch (err) {
       console.log("Error message:", err);
     }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -8,7 +8,17 @@ const signup = ({ email, password }) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
-  });
+  })
+    .then((res) => {
+      try {
+        if (res.status === 201) {
+          return res.json();
+        }
+      } catch (e) {
+        return e;
+      }
+    })
+    .catch((err) => console.log(err));
 };
 
 const signin = ({ email, password }) => {
@@ -34,3 +44,7 @@ const checkToken = (token) => {
 };
 
 export { BASE_URL, signup, signin, checkToken };
+
+// .then((data) => {
+//   localStorage.setItem("token", data.token);
+// })
